9f355619df0ee40a7a1693b57c7baa7bb34118f2
Ale <alejandro_beacon@hotmail.com>
2017-09-09 00:31:10 -0300

    First commit: Loading Library and early tests

---
+++ app/intervals.js
@@ -0,0 +1,134 @@
+const sonorities = {
+    "PerfectConsonances": [0, 5, 7, 12],
+    "NearConsonances": [3, 4],
+    "NearDisonances": [1, 2],
+    "FarConsonances": [8, 9],
+    "FarDisonances": [6, 10, 11],
+}
+
+const limits = {
+    "min" : 20,
+    "max" : 108
+}
+
+const maxLevel = 27
+
+
+var level = 0;
+var fundamental;
+var interval;
+var direction;
+var delay;
+//var stream = [];
+var exercise = [];
+
+function response(e){
+    if(e == Math.abs(interval)){
+        selectInterval();
+    }
+}
+
+function selectInterval(){
+    //Se define que intervalos se van a presentar
+    exercise = setIntervalLevel(level);
+    //En base al nivel se especifica si es ascendente o descendente, armónico o melódico
+    delay = setTiming(level);
+    direction = setDirection(level, delay);
+    //Se selecciona el intervalo
+    interval = exercise[Math.floor(Math.random() * exercise.length)];
+    interval *= direction;
+    //Se elige la fundamental en base a los límites y el intervalo elegido
+    fundamental = setFundamental(interval);
+    //Se cre la línea melódica y
+    //buildStream();
+    //Se manda a reproducir
+    loadOnBuffer(buildStream());
+}
+
+function buildStream(){
+    var stream = [];
+    stream[0] = new Note(fundamental,
+        0,
+        1);
+    stream[1] = new Note(fundamental + (interval * direction),
+        0 + delay,
+        1);
+    return stream;
+}
+
+function setFundamental(distance){
+    var scope = limits.max - limits.min - Math.abs(distance);    
+    var note = Math.floor(Math.random() * scope)+ limits.min;    
+    if(distance < 0) note += Math.abs(distance);
+    return note;
+}
+
+function setTiming(lvl){
+    if(lvl == maxLevel || lvl % 4 == 3) return Math.floor(Math.random()*2);
+    if(lvl % 4 < 2) return 1;
+    return 0;
+}
+
+function setDirection(lvl, del){
+    if(del == 0 || lvl%4 == 0) return 1;
+    else if(lvl%4 == 1) return -1;
+    return (Math.floor(Math.random()*2) - 0.5) * 2;
+    
+}
+
+function setIntervalLevel(lvl){
+    var levelStructure = [];
+    switch(Math.floor(lvl / 4)){
+            //Consonancias perfectas
+        case 0:
+            return levelStructure.concat(sonorities.PerfectConsonances);
+            break;
+        case 1:
+            //Consonancias cercanas
+            return levelStructure.concat(sonorities.NearConsonances);
+            break;
+        case 2:
+            //Consonancias perfectas y //Consonancias cercanas
+            return levelStructure.concat(sonorities.PerfectConsonances)
+                .concat(sonorities.NearConsonances)
+            return levelStructure
+            break;
+            //Disonancias cercanas
+        case 3:
+            return levelStructure.concat(sonorities.NearDisonances);
+            break;
+            //Consonancias perfectas, consonancias cercanas y disonancias cercanas
+        case 4:
+            return levelStructure.concat(sonorities.PerfectConsonances)
+                .concat(sonorities.NearConsonances)
+                .concat(sonorities.NearDisonances);
+            break;
+            //Consonancias lejanas
+        case 5:
+            return levelStructure.concat(sonorities.FarConsonances);
+            break;
+        case 6:
+            return levelStructure.concat(sonorities.PerfectConsonances)
+                .concat(sonorities.NearConsonances)
+                .concat(sonorities.NearDisonances)
+                .concat(sonorities.FarConsonances);
+            break;
+            //Disonancias lejanas
+        case 7:
+            return levelStructure.concat(sonorities.FarDisonances);
+            break;
+            //Todos los intervalos dentro de la octava
+            //Se devuelve un valor random directamente, el ramdom de la llamada 
+            //posterior solo podrá tomar el valor que retorne ésta función
+        case 8:
+            return [Math.floor(Math.random()*13)]
+            break;
+            //Intervalos compuestos
+        case 9:
+            return [Math.floor(Math.random()*25)]
+            break;
+            //Todo el puto registro sonoro
+        default:
+            break;
+    }
+}
---
+++ app/sequencer.js
@@ -0,0 +1,42 @@
+var bpm;
+var quarter = 60/bpm;
+var sequence = [];
+
+
+MIDI.loadPlugin({
+    soundfontUrl: "../MIDI.js/soundfont/",
+    instrument: "acoustic_grand_piano",
+    onprogress: function(state, progress) {
+        console.log(state, progress);
+    },
+    onsuccess: function() {
+        selectInterval();        
+    }
+});
+
+function loadOnBuffer(stream){
+    sequence.length = 0;
+    sequence.push(stream);
+    console.log(sequence)
+    playSequence();
+}
+
+function stop(){
+    MIDI.stopAllNotes();
+}
+
+function playSequence(){
+    MIDI.setVolume(0, 127);
+    for(var inst = 0; inst < sequence.length; inst++){
+        for(var i = 0; i < sequence[inst].length; i++){
+            MIDI.noteOn(sequence[inst][i].channel,
+                sequence[inst][i].note,
+                sequence[inst][i].dynamic,
+                sequence[inst][i].timeInPulse);
+            MIDI.noteOff(sequence[inst][i].channel,
+                sequence[inst][i].note,
+                sequence[inst][i].noteOff);
+      
+        }
+    }   
+}
